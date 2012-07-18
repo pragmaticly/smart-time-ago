5e5793f592333ff73402de43350f246ed9cde481
Terry Tai <poshboytl@gmail.com>
2012-07-19 01:32:31 +0800

    init commit.

---
+++ test/specs/timeago_spec.js
@@ -0,0 +1,220 @@
+describe("TimeAgo", function(){
+  beforeEach(function(){
+    TimeAgo = $.fn.timeago.Constructor
+    timeAgo = new TimeAgo("time.timeago", {})
+  });
+
+  describe("constructor", function(){
+    it("should contain the startInterval", function(){
+      expect(timeAgo.startInterval).toEqual(30000);
+    });
+  });
+
+  describe("#init", function(){
+    it("should set $element", function(){
+      timeAgo.init("time.timeago", {});
+      expect(timeAgo.$element).toEqual($("time.timeago"));
+    });
+
+    it("should load the options", function(){
+      spyOn($, 'extend').andReturn("spy");
+      timeAgo.init("time.timeago", {});
+      expect(timeAgo.options).toEqual("spy");
+    });
+
+    it("should call updateTime", function(){
+      spyOn(timeAgo, 'updateTime');
+      timeAgo.init("time.timeago", {});
+      expect(timeAgo.updateTime).toHaveBeenCalled();
+    });
+
+    it("should call startTimer", function(){
+      spyOn(timeAgo, "startTimer");
+      timeAgo.init("time.time", {});
+      expect(timeAgo.startTimer).toHaveBeenCalled();
+    });
+  });
+
+  describe("#startTimer", function(){
+    it("should call setInterval", function(){
+      spyOn(window, 'setInterval');
+      timeAgo.startTimer();
+      expect(window.setInterval).toHaveBeenCalled();
+    });
+
+    it("should set interval", function(){
+      spyOn(window, 'setInterval').andReturn("spy");
+      timeAgo.startTimer();
+      expect(timeAgo.interval).toEqual("spy");
+    });
+  });
+
+  describe("#stopTimer",function(){
+    it("should call clearInterval", function(){
+      spyOn(window, 'clearInterval');
+      timeAgo.stopTimer();
+      expect(window.clearInterval).toHaveBeenCalled();
+    });
+  });
+
+  describe("#restartTimer", function(){
+    it("should call stopTimer", function(){
+      spyOn(timeAgo, 'stopTimer');
+      timeAgo.restartTimer();
+      expect(timeAgo.stopTimer).toHaveBeenCalled();
+    });
+
+    it("should call startTimer", function(){
+      spyOn(timeAgo, 'startTimer');
+      timeAgo.restartTimer();
+      expect(timeAgo.startTimer).toHaveBeenCalled();
+    });
+  });
+
+  describe("#updateTime", function(){
+    beforeEach(function(){
+      timeLabel = $('<time class="timeago" datetime="2012-07-18T07:51:50Z">about 8 hours ago</time>')
+      $('body').append(timeLabel)
+      timeAgo = new TimeAgo("time.timeago", {selector: 'time.timeago', attr: 'datetime', dir: 'up', suffix: 'ago'});
+    });
+
+    afterEach(function(){
+      timeLabel.remove()
+    });
+
+    it("should update the relative time in html", function(){
+      spyOn(timeAgo, 'timeAgoInWords').andReturn('about 9 hours ago')
+      timeAgo.updateTime();
+      expect($('time.timeago').first().html()).toEqual("about 9 hours ago")
+    });
+  });
+
+  describe("#updateInterval", function(){
+    beforeEach(function(){
+      timeLabel = $('<time class="timeago" datetime="2012-07-18T07:51:50Z">about 8 hours ago</time>')
+      $('body').append(timeLabel)
+      timeAgo = new TimeAgo("time.timeago", {selector: 'time.timeago', attr: 'datetime', dir: 'up', suffix: 'ago'});
+    });
+
+    afterEach(function(){
+      timeLabel.remove()
+    });
+
+    describe("context: newestTimeInMinutes < 44.5 and @startInterval != 30000", function(){
+      beforeEach(function(){
+        timeAgo.startInterval = 60000 * 15
+      });
+
+      it("should update interval to 30000", function(){
+        spyOn(timeAgo, 'getTimeDistanceInMinutes').andReturn(44);
+        timeAgo.updateInterval();
+        expect(timeAgo.startInterval).toEqual(30000);
+      });
+
+      it("should call restartTimer", function(){
+        spyOn(timeAgo, 'getTimeDistanceInMinutes').andReturn(44);
+        spyOn(timeAgo, 'restartTimer');
+        timeAgo.updateInterval();
+        expect(timeAgo.restartTimer).toHaveBeenCalled();
+      });
+    });
+
+
+    describe("context: newestTimeInMinutes >= 44.5 and newestTimeInMinutes < 89.5 and @startInterval != 60000 * 15", function(){
+      beforeEach(function(){
+        timeAgo.startInterval = 30000;
+      });
+
+      it("should update interval to 900000", function(){
+        spyOn(timeAgo, 'getTimeDistanceInMinutes').andReturn(50);
+        timeAgo.updateInterval();
+        expect(timeAgo.startInterval).toEqual(900000);
+      });
+
+      it("should call restartTimer", function(){
+        spyOn(timeAgo, 'getTimeDistanceInMinutes').andReturn(50);
+        spyOn(timeAgo, 'restartTimer');
+        timeAgo.updateInterval();
+        expect(timeAgo.restartTimer).toHaveBeenCalled();
+      });
+    });
+
+    describe("context: newestTimeInMinutes >= 89.5 and newestTimeInMinutes < 1439.5 and @startInterval != 60000 * 30", function(){
+      beforeEach(function(){
+        timeAgo.startInterval = 900000;
+      });
+
+      it("should update interval to 1800000", function(){
+        spyOn(timeAgo, 'getTimeDistanceInMinutes').andReturn(500);
+        timeAgo.updateInterval();
+        expect(timeAgo.startInterval).toEqual(1800000);
+      });
+
+      it("should call restartTimer", function(){
+        spyOn(timeAgo, 'getTimeDistanceInMinutes').andReturn(500);
+        spyOn(timeAgo, 'restartTimer');
+        timeAgo.updateInterval();
+        expect(timeAgo.restartTimer).toHaveBeenCalled();
+      });
+    });
+
+  });
+
+
+  describe("#parse", function(){
+    beforeEach(function(){
+      timeStr = "2012-07-18T07:51:50Z";
+      result = timeAgo.parse(timeStr);
+    });
+
+    it("should get the right time", function(){
+      expect(result.toUTCString()).toEqual("Wed, 18 Jul 2012 07:51:50 GMT");
+    });
+  });
+
+  describe("distanceOfTimeInWords", function(){
+    describe("context: dim >= 0 and dim < 0.5", function(){
+      it("should return 'less than a minute'", function(){
+        expect(timeAgo.distanceOfTimeInWords(0.1)).toEqual("less than a minute");
+      });
+    });
+
+    describe("context:  dim >= 0.5 and dim < 1.5", function(){
+      it("should return '1 minute'", function(){
+        expect(timeAgo.distanceOfTimeInWords(0.6)).toEqual("1 minute");
+      });
+    });
+
+    describe("context: dim >= 1.5 and dim < 44.5", function(){
+      it("should return '40 minutes'", function(){
+        expect(timeAgo.distanceOfTimeInWords(40)).toEqual("40 minutes");
+      });
+    });
+
+    describe("context:  dim >= 44.5 and dim < 89.5", function(){
+      it("should return 'about 1 hour'", function(){
+        expect(timeAgo.distanceOfTimeInWords(80)).toEqual("about 1 hour");
+      });
+    });
+
+    describe("context: dim >= 89.5 and dim < 1439.5", function(){
+      it("should return 'about 2 hours'", function(){
+        expect(timeAgo.distanceOfTimeInWords(120)).toEqual("about 2 hours");
+      });
+    });
+
+    describe("context: dim >= 1439.5 and dim < 2519.5", function(){
+      it("should return '1 day'", function(){
+        expect(timeAgo.distanceOfTimeInWords(1500)).toEqual("1 day");
+      });
+    });
+
+    describe("context: dim >= 2519.5 and dim < 43199.5", function(){
+      it("should return '3 days'", function(){
+        expect(timeAgo.distanceOfTimeInWords(4321)).toEqual("3 days");
+      });
+    });
+
+  });
+
+});
---
+++ timeago.coffee
@@ -0,0 +1,132 @@
+class TimeAgo
+  constructor: (element, options) ->
+    @startInterval = 30000
+    @init(element, options)
+
+  init: (element, options) ->
+    @$element = $(element)
+    @options = $.extend({}, $.fn.timeago.defaults, options)
+    @updateTime()
+    @startTimer()
+
+  startTimer: ->
+    self = @
+    @interval = setInterval ( ->
+      self.refresh()
+    ), @startInterval
+
+  stopTimer: ->
+    clearInterval(@interval)
+
+  restartTimer: ->
+    @stopTimer()
+    @startTimer()
+
+  refresh: ->
+    @updateTime()
+    @updateInterval()
+
+  updateTime: ->
+    self = @
+    @$element.findAndSelf(@options.selector).each ->
+      timeAgoInWords = self.timeAgoInWords($(this).attr(self.options.attr))
+      $(this).html(timeAgoInWords)
+
+  updateInterval: ->
+    if @$element.findAndSelf(@options.selector).length > 0
+      if @options.dir is "up"
+        filter = ":first"
+      else if @options.dir is "down"
+        filter = ":last"
+      newestTimeSrc = @$element.findAndSelf(@options.selector).filter(filter).attr(@options.attr)
+      newestTime = @parse(newestTimeSrc)
+      newestTimeInMinutes = @getTimeDistanceInMinutes(newestTime)
+      if newestTimeInMinutes < 44.5 and @startInterval != 30000
+        @startInterval = 30000
+        @restartTimer()
+      else if newestTimeInMinutes >= 44.5 and newestTimeInMinutes < 89.5 and @startInterval != 60000 * 15
+        @startInterval = 60000 * 15
+        @restartTimer()
+      else if newestTimeInMinutes >= 89.5 and newestTimeInMinutes < 1439.5 and @startInterval != 60000 * 30
+        @startInterval = 60000 * 30
+        @restartTimer()
+
+  timeAgoInWords: (timeString) ->
+    absolutTime = @parse(timeString)
+    distanceInMinutes = @getTimeDistanceInMinutes(absolutTime)
+    @distanceOfTimeInWords(distanceInMinutes) + " #{@options.suffix}"
+
+  parse: (iso8601) ->
+    timeStr = $.trim(iso8601)
+    timeStr = timeStr.replace(/\.\d\d\d+/,"")
+    timeStr = timeStr.replace(/-/,"/").replace(/-/,"/")
+    timeStr = timeStr.replace(/T/," ").replace(/Z/," UTC")
+    timeStr = timeStr.replace(/([\+\-]\d\d)\:?(\d\d)/," $1$2")
+    new Date(timeStr);
+
+  getTimeDistanceInMinutes: (absolutTime) ->
+    timeDistance = new Date().getTime() - absolutTime.getTime()
+    (Math.abs(timeDistance) / 1000) / 60
+
+  distanceOfTimeInWords: (dim) ->
+    # Wrods need to be the same with Rails
+    # 0 <-> 29 secs                                                             # => less than a minute
+    # 30 secs <-> 1 min, 29 secs                                                # => 1 minute
+    # 1 min, 30 secs <-> 44 mins, 29 secs                                       # => [2..44] minutes
+    # 44 mins, 30 secs <-> 89 mins, 29 secs                                     # => about 1 hour
+    # 89 mins, 30 secs <-> 23 hrs, 59 mins, 29 secs                             # => about [2..24] hours
+    # 23 hrs, 59 mins, 30 secs <-> 41 hrs, 59 mins, 29 secs                     # => 1 day
+    # 41 hrs, 59 mins, 30 secs  <-> 29 days, 23 hrs, 59 mins, 29 secs           # => [2..29] days
+    # 29 days, 23 hrs, 59 mins, 30 secs <-> 59 days, 23 hrs, 59 mins, 29 secs   # => about 1 month
+    # 59 days, 23 hrs, 59 mins, 30 secs <-> 1 yr minus 1 sec                    # => [2..12] months
+    # 1 yr <-> 1 yr, 3 months                                                   # => about 1 year
+    # 1 yr, 3 months <-> 1 yr, 9 months                                         # => over 1 year
+    # 1 yr, 9 months <-> 2 yr minus 1 sec                                       # => almost 2 years
+    # 2 yrs <-> max time or date                                                # => (same rules as 1 yr)
+
+    if dim >= 0 and dim < 0.5
+      "less than a minute"
+    else if dim >= 0.5 and dim < 1.5
+      "1 minute"
+    else if dim >= 1.5 and dim < 44.5
+      "#{ Math.round(dim) } minutes"
+    else if dim >= 44.5 and dim < 89.5
+      "about 1 hour"
+    else if dim >= 89.5 and dim < 1439.5 #23 hrs, 59 mins, 29 secs
+      "about #{ Math.round(dim / 60) } hours"
+    else if dim >= 1439.5 and dim < 2519.5 #41 hrs, 59 mins, 29 secs
+      "1 day"
+    else if dim >= 2519.5 and dim < 43199.5 #29 days, 23 hrs, 59 mins, 29 secs
+      "#{ Math.round(dim / 1440) } days"
+    else if dim >= 43199.5 and dim < 86399.5 #59 days, 23 hrs, 59 mins, 29 secs
+      "about 1 month"
+    else if dim >= 86399.5 and dim < 525599.5 #1 yr minus half minute
+      "#{ Math.round(dim / 43200) } months"
+    else if dim >= 525599.5 and dim < 655199.5 #1 yr, 3 months
+      "about 1 year"
+    else if dim >= 655199.5 and dim < 914399.5 #1 yr, 9 months
+      "over 1 year"
+    else if dim >= 914399.5 and dim < 1051199.5 #2 yr minus half minute
+      "almost 2 years"
+    else
+      "about #{Math.round(dim / 525600)} years"
+
+$.fn.timeago = (options = {}) ->
+  @each ->
+    $this = $(this)
+    data = $this.data("timeago")
+    if (!data)
+      $this.data("timeago", new TimeAgo(this, options))
+    if (typeof options is 'string')
+      data[options]()
+
+$.fn.findAndSelf = (selector) ->
+  this.find(selector).add(this.filter(selector))
+
+$.fn.timeago.Constructor = TimeAgo
+
+$.fn.timeago.defaults =
+  selector: 'time.timeago'
+  attr: 'datetime'
+  dir: 'up'
+  suffix: 'ago'
